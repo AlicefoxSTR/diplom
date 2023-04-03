b3e774f7caaf825833372a1e12b0df86aa48f9ba
AlicefoxSTR <vishaaaffffff@mail.ru>
2023-04-03 19:49:40 +0300

    initialize back part

---
+++ back/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+
+
+
+
+
+
+class HomeView(ListAPIView):
+
+    pass
---
+++ back/config/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .yasg import urlpatterns as doc_urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include('api.urls')),
+]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += doc_urls
