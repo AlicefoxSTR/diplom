47e313fc34dd8f01bd65b1763b35611f2b673621
AlicefoxSTR <vishaaaffffff@mail.ru>
2023-06-07 17:37:05 +0400

    edited nginx.config

--- front/src/shared/lib/ApiConfig/ApiConfig.js
+++ front/src/shared/lib/ApiConfig/ApiConfig.js
@@ -1,4 +1,5 @@
 
 // export const baseApiUrl = 'http://localhost:8000/api'
 
-export const baseApiUrl = 'http://192.168.0.176:8000/api'
+// export const baseApiUrl = 'http://192.168.0.176:8000/api'
+export const baseApiUrl = 'http://93.159.221.90/api'
